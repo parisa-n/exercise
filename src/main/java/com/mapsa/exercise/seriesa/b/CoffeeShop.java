38f60af086a2cfbaf99107156b19272bfc96102b
monajahromi <jahromi.mona66@gmail.com>
2022-09-29 15:04:40 +0330

    first commit

--- src/main/java/com/mapsa/exercise/seriesa/b/CoffeeShop.java
+++ src/main/java/com/mapsa/exercise/seriesa/b/CoffeeShop.java
@@ -1,6 +1,35 @@
 package com.mapsa.exercise.seriesa.b;
 
 public class CoffeeShop {
+
+	public CoffeeShop(String string, MenuItem[] menu, String[] strings) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object addOrder(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double dueAmount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Object listOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object fulfillOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object cheapestItem() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
 
